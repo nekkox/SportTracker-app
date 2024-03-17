99b5495aba010e66c8d7a3344cc40c717904297e
nekkox <klascibox@gmail.com>
2024-03-17 23:34:08 +0100

    Create AppStore for layout options

--- src/store/app.js
+++ src/store/app.js
@@ -1,8 +1,55 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import router from "@/router";
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
-})
+export const useAppStore = defineStore("app", () => {
+  const drawer = ref(false);
+
+  const toggleDrawer = (newValue) => {
+    if (typeof newValue === "boolean") {
+      drawer.value = newValue;
+    } else {
+      drawer.value = !drawer.value;
+    }
+  };
+
+  const pageTitle = ref("Home");
+
+  const navigateToPage = (page) => {
+    const title = page.slice(1).charAt(0).toUpperCase() + page.slice(2);
+    pageTitle.value = title || "Home";
+    toggleDrawer(false);
+    router.push(page);
+  };
+
+  const dialog = ref({
+    title: "",
+    contents: "",
+    visible: false,
+    fullscreen: false,
+  });
+
+  const showDialog = (options) => {
+    dialog.value = { ...options, visible: true };
+  };
+
+  const hideDialog = () => {
+    dialog.value = {
+      title: "",
+      contents: "",
+      visible: false,
+      fullscreen: false,
+    };
+  };
+
+  return {
+    drawer,
+    pageTitle,
+    toggleDrawer,
+    navigateToPage,
+    dialog,
+    showDialog,
+    hideDialog,
+  };
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,8 @@
 <script setup>
  import Form from '@/components/FormLogin.vue'
+import { onMounted, ref } from 'vue';
+
+
 </script>
 
 <template>
