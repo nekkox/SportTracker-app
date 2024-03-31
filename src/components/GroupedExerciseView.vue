85e6e2e42549bfef4986fbd711a1c478f126dc00
nekkox <klascibox@gmail.com>
2024-03-31 16:31:28 +0200

    Create ExerciseGrouping.vue component

---
+++ src/components/GroupedExerciseView.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+defineProps({
+  exercise: {
+    type: Object,
+    required: true,
+  },
+});
+
+</script>
+<template>
+  <v-col cols="12">
+    <v-row>
+        <v-col cols="12"><h5 class="text-h5">{{ exercise[0].exercise_name }}</h5></v-col>
+    </v-row>
+    <v-row v-for="(routine, index) in exercise" :key="index">
+      <v-col cols="6">{{ routine.sets_weight }}</v-col>
+      <v-col cols="6">{{ routine.sets_repetitions }}</v-col>
+    </v-row>
+  </v-col>
+</template>
--- src/components/History.vue
+++ src/components/History.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useFitnessStore } from "@/store/fitness";
 const fitnessStore = useFitnessStore();
@@ -7,6 +7,7 @@ const fitnessStore = useFitnessStore();
 const { workouts } = storeToRefs(fitnessStore);
 
 import WorkoutStats from "./WorkoutStats.vue";
+import GroupedExerciseView from "./GroupedExerciseView.vue"
 
 const panel = ref([0]);
 
@@ -22,6 +23,32 @@ const formattedDate = (date) => {
   return "";
 };
 
+//every unique workout has a collection of corresponding exercises as a child
+const workoutsById = (workouts = []) =>
+  workouts.reduce((acc, curr) => {
+    if (!acc[curr.workout_id])
+      acc[curr.workout_id] = {
+        workout_id: curr.workout_id,
+        workout_created_at: curr.workout_created_at,
+        sets: [],
+      };
+    acc[curr.workout_id].sets.push(curr);
+    return acc;
+  }, {});
+
+  const setsByExerciseName = (sets) =>
+  sets.reduce((acc, curr) => {
+    if (!acc[curr.exercise_name]) acc[curr.exercise_name] = [];
+    acc[curr.exercise_name].push(curr);
+    return acc;
+  }, {});
+
+const workoutIds = computed(() => Object.keys(workoutsById(workouts.value)));
+
+const workoutsGroupedById = computed(() => workoutsById(workouts.value));
+
+
+
 onMounted(() => {
   fitnessStore.getWorkouts({ order: 'descending' });
 });
@@ -29,13 +56,18 @@ onMounted(() => {
 
 <template>
   <v-container>
-    <workout-stats class="mb-4" />
+    <WorkoutStats class="mb-4" />
       <h1>Past workouts</h1>
       <v-expansion-panels v-model="panel" multiple v-if="workouts">
         <v-expansion-panel v-for="workout in workouts" :key="workout.workout_id">
           <v-expansion-panel-title>{{ formattedDate(new Date(workout.workout_created_at)) }}</v-expansion-panel-title>
           <v-expansion-panel-text>
-          ...
+            
+            <GroupedExerciseView 
+            :exercise="set" 
+            v-for="(set, index) in 
+            setsByExerciseName(workoutsGroupedById[id].sets)" :key="index" />
+
           </v-expansion-panel-text>
         </v-expansion-panel>
       </v-expansion-panels>
